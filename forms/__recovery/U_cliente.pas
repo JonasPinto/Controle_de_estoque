5fd359306eb1480f1d89aed6ceb8e426780c0340
Jonas Silva <jonas.silva@easolucinfo.com>
2023-08-03 21:09:43 -0300

    Update U_cliente.pas

--- forms/__recovery/U_cliente.pas
+++ forms/__recovery/U_cliente.pas
@@ -12,6 +12,42 @@ uses
 
 type
   TFrm_cliente = class(TFrm_padrao)
+    q_padraoID_CLIENTE: TFDAutoIncField;
+    q_padraoNOME: TStringField;
+    q_padraoENDERECO: TStringField;
+    q_padraoNUMERO: TIntegerField;
+    q_padraoBAIRRO: TStringField;
+    q_padraoCIDADE: TStringField;
+    q_padraoUF: TStringField;
+    q_padraoCEP: TStringField;
+    q_padraoTELEFONE: TStringField;
+    q_padraoCPF: TStringField;
+    q_padraoEMAIL: TStringField;
+    q_padraoCADASTRO: TDateField;
+    Label1: TLabel;
+    DBEdit1: TDBEdit;
+    Label2: TLabel;
+    DBEdit2: TDBEdit;
+    Label3: TLabel;
+    DBEdit3: TDBEdit;
+    Label4: TLabel;
+    DBEdit4: TDBEdit;
+    Label5: TLabel;
+    DBEdit5: TDBEdit;
+    Label6: TLabel;
+    DBEdit6: TDBEdit;
+    Label7: TLabel;
+    DBEdit7: TDBEdit;
+    Label8: TLabel;
+    DBEdit8: TDBEdit;
+    Label9: TLabel;
+    DBEdit9: TDBEdit;
+    Label10: TLabel;
+    DBEdit10: TDBEdit;
+    Label11: TLabel;
+    DBEdit11: TDBEdit;
+    Label12: TLabel;
+    DBEdit12: TDBEdit;
   private
     { Private declarations }
   public
